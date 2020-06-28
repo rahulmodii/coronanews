18cab5cc159062d89023b2255c11fec60a5f0100
rahul <rahul@rahuls-MacBook-Pro.local>
2020-06-28 22:48:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { MdLocalHospital } from 'react-icons/md';
+import Axios from 'axios';
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 function App() {
+  const [data, setData] = useState([]);
+  const datafetch=  Axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=iKtuOuUa3HpRNT29EU158-zDONmfGbj1oxMiNzMqeVmayEnkjBrhgmsOOQsoh32jBArpLUYqvFJrwvmCIdlNho0cMSo1UCxtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGiWWYOmJ9h6lZ_IpSxPc3Mk724--L1TfC1Z-y73voLiJjrPI0e1zFD-zJpYRgYIi4IvOk0K8afJ&lib=MQsFhc4Q4dg8H3KYC4GFNrC8rVciQZiLg').then(res=>setData(res.data.user))
+  
+
+  if (!data.length) {
+  return  <div style={{position:'fixed',top:'50%',left:'50%' ,marginTop: '-50px',marginLeft: '-100px'}} >
+      <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} //3 secs
+ 
+      />
+  </div>
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div style={{ backgroundColor: 'grey' }}>
+      <VerticalTimeline className="vertical-timeline-custom-line">
+        {data.map((u,i)=>{
+             return <VerticalTimelineElement
+                  className="vertical-timeline-element--work"
+                  contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                  contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                  date={u.date}
+                  iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                  icon={<MdLocalHospital />}
+                  key={i}
+                >
+                  <h3 className="vertical-timeline-element-title">Corona News</h3>
+                  <img src={u.image} alt="Logo" width='100%' />
+                  <p>
+                   {u.description}
+                  </p>
+                </VerticalTimelineElement>
+        })}
+
+
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          icon={<MdLocalHospital />}
+        />
+      </VerticalTimeline>
     </div>
+
   );
 }
 
